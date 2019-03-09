39a0920b50f8bfb7bec9afd8d525917a72aaa8a0
hnguyen <hung135@hotmail.com>
2019-03-09 17:54:57 -0500

    start to break out data into yml and ansible file

--- deploy/inventory.rb
+++ deploy/inventory.rb
@@ -7,68 +7,74 @@
 #   end
 # end
 class Inventory
-  def extract_ipv4(line)
-    ip_regex=/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/
-    ip=line[ip_regex]
-    return ip
-  end
-  def get_hostname(line)
-    return line.split[0]
-  end
+    def extract_ipv4(line)
+        ip_regex = /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/
+        ip = line[ip_regex]
+        ip
+    end
 
-  #return a dict with all groups passed in
-  def get_hosts_by_groups(group_list)
+    def extract_additional_storage(line)
+        storage = 0
+        storage_str = line[/(additional_storage=\d\d\d\d)/]
+        storage = storage_str.split('=')[1] unless storage_str.nil?
+        storage.to_i
+    end
 
-      inventory={}
-      for group in group_list do
-          inventory[group]=@inventory[group]
-      end
-      return inventory
-  end
-  def initialize(file_path="./vagrant_hosts")
+    def get_hostname(line)
+        line.split[0]
+    end
 
-      File.open(file_path, "r") do |f|
-          dictionary={}
-          first_group_found=false
-          curr_dict={}
-          array_item=[]
-          f.each_line do |line|
+    # return a dict with all groups passed in
+    def get_hosts_by_groups(group_list)
+        inventory = {}
+        group_list.each do |group|
+            inventory[group] = @inventory[group]
+        end
+        inventory
+    end
 
-              is_group=  /\[.*\]/=~line
+    def initialize(file_path = './vagrant_hosts')
+        File.open(file_path, 'r') do |f|
+            dictionary = {}
+            first_group_found = false
+            curr_dict = {}
+            array_item = []
+            f.each_line do |line|
+                is_group = /\[.*\]/=~line
 
-              if first_group_found!=true and is_group then
-                  #find the first group
-                  first_group_found=true
-                  idx=line.strip.delete("[]")
+                if (first_group_found != true) && is_group
+                    # find the first group
+                    first_group_found = true
+                    idx = line.strip.delete('[]')
 
-                  curr_dict[idx]=array_item
-              elsif is_group then
-                  dictionary=dictionary.merge(curr_dict)
+                    curr_dict[idx] = array_item
+                elsif is_group
+                    dictionary = dictionary.merge(curr_dict)
 
-                  curr_dict={}
-                  array_item=[]
-                  idx=line.strip.delete("[]")
-                  curr_dict[idx]=array_item
-              else
+                    curr_dict = {}
+                    array_item = []
+                    idx = line.strip.delete('[]')
+                    curr_dict[idx] = array_item
+                else
 
-                  ipv4 =extract_ipv4(line)
+                    ipv4 = extract_ipv4(line)
+                    additional_storage = extract_additional_storage(line)
+                    if ipv4
+                        hostname = get_hostname(line)
 
-                  if ipv4 then
-                      hostname=get_hostname(line)
-                      array_item.push({hostname:hostname,ipv4:ipv4})
-                  end
-              end
-          end
-          @inventory={}
-          dictionary=dictionary.merge(curr_dict)
-          dictionary.each do |key, value|
-              if value.length>0 then
-                  @inventory[key]=value
-                  #puts (key + "\n\t-->" + value*",")
-              end
-          end
-
-      end
-  end
-
+                        array_item.push(hostname: hostname, ipv4: ipv4, additional_storage: additional_storage)
+                     
+                    end
+                end
+            end
+            @inventory = {}
+            dictionary = dictionary.merge(curr_dict)
+            dictionary.each do |key, value|
+                unless value.empty?
+                    @inventory[key] = value
+                    # puts (key + "\n\t-->" + value*",")
+                end
+            end
+        end
+    end
 end
