662299417ca06dda94382e94da3482dfe6aac04b
EC2 Default User <ec2-user@ip-172-31-0-208.ec2.internal>
2019-03-12 14:29:41 +0000

    checking in work

--- environment_setup.sh
+++ environment_setup.sh
@@ -4,10 +4,16 @@ cd /etc/yum.repos.d/
 sudo wget http://download.opensuse.org/repositories/shells:fish:release:3/RHEL_7/shells:fish:release:3.repo
 sudo yum install -y fish 
 sudo yum install -y https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.rpm
-sudo yum install epel-release
+sudo yum install -y ansible 
 
 git clone https://github.com/hung135/aurora
-export keypair_name=$HOSTNAME
-aws --region us-east-1 ec2 delete-key-pair --key-name $keypair_name
-aws --region us-east-1 ec2 create-key-pair --key-name "$keypair_name" --output text > ~/.ssh/$keypair_name.pem
-chmod 400 ~/.ssh/$keypair_name.pem
+export key_pair_name=$HOSTNAME
+chmod 600 ~/.ssh/$key_pair_name.pem
+aws --region us-east-1 ec2 delete-key-pair --key-name $key_pair_name
+aws --region us-east-1 ec2 create-key-pair --key-name "$key_pair_name" --output text > ~/.ssh/$key_pair_name.pem
+chmod 400 ~/.ssh/$key_pair_name.pem
+cp ~/.ssh/$key_pair_name.pem ~/.ssh/$key_pair_name.pem_bk
+vagrant box add aws_dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
+vagrant plugin install vagrant-aws
+vagrant plugin install vagrant-vbguest 
+echo abc >deploy/.vault.password
